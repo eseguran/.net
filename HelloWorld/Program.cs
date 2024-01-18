1e801aa193bc97e7d6dbf00d89f30d18b3627158
Eddy Segura <eseguran@outlook.com>
2024-01-17 20:38:07 -0600

    ok

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 Console.WriteLine("What is your name?");
 var name = Console.ReadLine();
 Console.WriteLine($"{Environment.NewLine} Hello, {name}!");
