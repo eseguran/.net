a6c7d090183649cb88ac229b0695c6854d0e0b6a
Eddy Segura <eseguran@outlook.com>
2024-01-17 20:35:47 -0600

    init

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"{Environment.NewLine} Hello, {name}!");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
